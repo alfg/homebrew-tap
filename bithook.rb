6775d2d327b255d5e1e8d11750c413d67b44ca03
Alfred Gutierrez <alfg@users.noreply.github.com>
2017-04-03 10:59:30 -0700

    Update bithook.rb

--- bithook.rb
+++ bithook.rb
@@ -1,9 +1,9 @@
 class Bithook < Formula
   desc "A Bitcoin Webhook CLI"
   homepage "https://github.com/alfg/bithook"
-  url "https://github.com/alfg/bithook/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "d7dd7a8341a61249d3569bfb73d5564fa7949a785e5a75ff7a86d62482c9ee1a"
+  url "https://github.com/alfg/bithook/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "1e6d21d93b40540f16aa1c46f957e9cdf6180e85e666a520fac887319c78a3d5"
 
   depends_on "go" => :build
 
