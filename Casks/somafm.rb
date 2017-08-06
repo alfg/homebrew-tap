251966a3a1242a54db945211fb57c6fa94ce08ef
Alfred Gutierrez <alfg@users.noreply.github.com>
2017-08-05 21:44:00 -0700

    Update somafm.rb

--- Casks/somafm.rb
+++ Casks/somafm.rb
@@ -1,10 +1,10 @@
 cask 'somafm' do
-  version '0.3.0'
-  sha256 'ca8cad7cb28ddcc4c6f3ba10d488c83af8745f966f439fca6246d539f22e802b'
+  version '0.3.1'
+  sha256 '1a80ca569aeb86bd4da88da31d134fc7c273e58ecc3645e2d2d6af2cd22cbd18'
 
   url "https://github.com/alfg/somafm/releases/download/v#{version}/SomaFM-#{version}.dmg"
   appcast 'https://github.com/alfg/somafm/releases.atom',
-          checkpoint: '5afaf0bfdb8b96964c507146d3e37d98d8a554ca10cb8fe62a3ff32c3f071fc9'
+          checkpoint: '17e2e5a28071c1cfa9ef1bf25053be2c6ad11626a755a2792c71a34c64f79ce6'
   name 'SomaFM'
   homepage 'https://github.com/alfg/somafm'
 
