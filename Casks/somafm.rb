b6136d7081332326f10fe0b2a0dd1ff7d2944a22
alf <alf.g.jr@gmail.com>
2017-11-07 21:31:02 -0800

    Update SomaFM app version.

--- Casks/somafm.rb
+++ Casks/somafm.rb
@@ -1,10 +1,10 @@
 cask 'somafm' do
-  version '0.3.1'
-  sha256 '1a80ca569aeb86bd4da88da31d134fc7c273e58ecc3645e2d2d6af2cd22cbd18'
+  version '0.3.2'
+  sha256 '23ed962c115da748425ed6416b78859de3d0b868b2c70957ffc7deb69003425b'
 
   url "https://github.com/alfg/somafm/releases/download/v#{version}/SomaFM-#{version}.dmg"
   appcast 'https://github.com/alfg/somafm/releases.atom',
-          checkpoint: '17e2e5a28071c1cfa9ef1bf25053be2c6ad11626a755a2792c71a34c64f79ce6'
+          checkpoint: 'aac0a20962dea46ae1c17ba7b014b98df2d2ee4c4fca5845d77e5db2ccabbebd'
   name 'SomaFM'
   homepage 'https://github.com/alfg/somafm'
 
